0a758bbb426e336a1ea18d6201701564cc356599
fadilmohammedv <fadilmohammedv@gmail.com>
2026-04-20 09:19:50 +0530

    UC2: Toss to Decide First Player and Symbol

---
+++ uc2.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss! You go first with: " + humanSymbol);
+            System.out.println("Computer will play second with: " + computerSymbol);
+        } else {
+            System.out.println("Computer won the toss! It goes first with: " + computerSymbol);
+            System.out.println("You will play second with: " + humanSymbol);
+        }
+    }
+}
